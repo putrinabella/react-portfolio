bc59785550773e244e27768ed366f1feb6ba0432
putrinabella <putrinabellaaa@gmail.com>
2024-09-29 22:35:31 +0800

    feat: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer footer-center bg-base-300 text-base-content p-4">
+    <footer className="footer footer-center bg-base-300 text-base-content p-4 font-bold">
       <aside>
         <p>&copy; 2024 | Putri Nabella [1959095840-46]</p>
       </aside>
